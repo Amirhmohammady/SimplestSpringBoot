af666ea241801495490f95d4f5a82154aa46047e
Amirhmohammady <amir_h_mohammady@yahoo.com>
2021-05-11 10:42:39 +0430

    json retuen in rest controller fo ajax call

--- src/main/java/com/mycompany/myapp/controller/AjaxRestController.java
+++ src/main/java/com/mycompany/myapp/controller/AjaxRestController.java
@@ -1,18 +1,33 @@
 package com.mycompany.myapp.controller;
 
-import org.springframework.stereotype.Controller;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Amir on 5/8/2021.
  */
-@Controller
+@RestController
 public class AjaxRestController {
     @GetMapping("/ajaxrequest")
-    public
-    @ResponseBody
-    String getSearchResultViaAjax() {
-        return "sibzamini";
+    public ResponseEntity<Object> getSearchResultViaAjax() {
+/*        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return new ResponseEntity<Object>(map, HttpStatus.OK);*/
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        JSONObject json = new JSONObject();
+        json.put("111","aaa");
+        entities.add(new JSONObject().put("111","aaa"));
+        entities.add(new JSONObject().put("222","bbb"));
+        return new ResponseEntity<Object>(entities.toString(), HttpStatus.OK);
     }
 }
